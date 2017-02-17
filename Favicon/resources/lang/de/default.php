fb364e025b0dec0fdeb5e4e65ebc559476d56264
Rémy <remy.boehler@subpixel.ch>
2017-02-17 16:12:34 +0100

    add german translation

---
+++ Favicon/resources/lang/de/default.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Rémy M. Böhler
+ */
+
+return [
+    'favicon' => 'Favicon',
+    'generate_favicon' => 'Favicon erstellen',
+    'api_error' => 'Es trat ein Fehler mit dem realfavicongenerator.com API auf',
+    'continue' => 'Weiter',
+    'redirect' => 'Weiterleitung',
+    'will_redirect' => 'Sie werden gleich weitergeleitet',
+    'favicon_updated' => 'Ihr Favicon wurde aktualisiert',
+    'current_favicon' => 'Ihr aktuelles Favicon',
+    'template_code' => 'Template Code',
+    'html_code' => 'HTML Code',
+    'remove_favicon' => 'Favicon entfernen',
+    'error_no_asset' => 'Bitte wählen Sie ein Bild',
+    'error_config' => 'Das Favicon Addon muss erst konfiguriert werden',
+];
---
+++ Favicon/resources/lang/de/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Rémy M. Böhler
+ */
+
+return [
+    'apikey' => 'API Key',
+    'apikey_instruct' => 'Der API Key kann auf <a href="https://realfavicongenerator.net/api/#register_key">Real Favicon Generator</a> erstellt werden',
+
+    'partial' => 'Partial',
+    'partial_instruct' => 'Name der Partial Datei in der der Code gespeichert wird',
+
+    'asset_container' => 'Assets container',
+    'asset_container_instruct' => 'Container für die Favicon Bilder',
+
+    'asset_folder' => 'Asset Verzeichnis',
+    'asset_folder_instruct' => 'Unterverzeichnis für die erstellten Favicons',
+];
